3b9463e1fcef2699d9e8ad5e8a53636b551c3b55
Alexander Vasilenko <a6461@yandex.ru>
2018-05-04 22:10:08 +0300

    Реализована 4я подглава для проекта CURSORS(C++)

---
+++ C++/I/07-CURSORS/4/form.h
@@ -0,0 +1,36 @@
+#ifndef FORM_H
+#define FORM_H
+
+#include <QWidget>
+
+namespace Ui {
+class Form;
+}
+
+class Form : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Form(QWidget *parent = 0);
+    ~Form();
+    QList<QString>* names();
+    QList<QCursor>* cursors();
+
+private slots:
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_4_clicked();
+
+    void on_pushButton_5_clicked();
+
+private:
+    Ui::Form *ui;
+    QList<QString> *str = new QList<QString>();
+    QList<QCursor> *cur = new QList<QCursor>();
+    QList<QIcon>* ico = new QList<QIcon>();
+};
+
+#endif // FORM1_H
