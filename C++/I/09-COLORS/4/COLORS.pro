3568b7f08b84b2e7ffe90089bf1b1577a6748433
Alexander Vasilenko <a6461@yandex.ru>
2018-05-05 23:50:10 +0300

    Реализована 4я подглава для проекта COLORS(C++)

---
+++ C++/I/09-COLORS/4/COLORS.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-05-23T09:39:56
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = COLORS
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        form.cpp
+
+HEADERS  += form.h
+
+FORMS    += form.ui
---
+++ C++/I/09-COLORS/4/form.cpp
@@ -0,0 +1,57 @@
+#include "form.h"
+#include "ui_form.h"
+
+Form::Form(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Form)
+{
+    ui->setupUi(this);
+    setFixedSize(size());
+
+    ui->label_6->setStyleSheet("background-color: black; color: white");
+    connect(ui->horizontalSlider_2, SIGNAL(valueChanged(int)), this, SLOT(on_horizontalSlider_valueChanged()));
+    connect(ui->horizontalSlider_3, SIGNAL(valueChanged(int)), this, SLOT(on_horizontalSlider_valueChanged()));
+    connect(ui->horizontalSlider_4, SIGNAL(valueChanged(int)), this, SLOT(on_horizontalSlider_valueChanged()));
+    on_horizontalSlider_valueChanged();
+    QFont font = ui->label_6->font();
+    font.setCapitalization(QFont::Capitalize);
+    ui->label_6->setFont(font);
+}
+
+Form::~Form()
+{
+    delete ui;
+}
+
+void Form::on_horizontalSlider_valueChanged()
+{
+    QColor c = QColor(ui->horizontalSlider_2->value(), ui->horizontalSlider_3->value(),
+        ui->horizontalSlider_4->value(), ui->horizontalSlider->value());
+    ui->label_6->setStyleSheet(QString("background-color: rgba(%1,%2,%3,%4); color: rgb(%5,%6,%7)")
+        .arg(c.red()).arg(c.green()).arg(c.blue()).arg(c.alpha() / 255.0)
+        .arg(255 ^ c.red()).arg(255 ^ c.green()).arg(255 ^ c.blue()));
+    ui->label_6->setText(QString("%1%2").arg(QString::number(c.alpha(), 16).toUpper(), 2, '0')
+        .arg(c.name().remove(0, 1).toUpper()));
+    switch (c.alpha())
+    {
+        case 0:
+            ui->label_6->setText(ui->label_6->text() + " Transparent");
+            break;
+        case 255:
+            foreach (QString color, QColor::colorNames())
+                if (QColor(color).rgb() == c.rgb())
+                {
+                    ui->label_6->setText(ui->label_6->text() + " " + color);
+                    break;
+                }
+            break;
+    }
+}
+
+void Form::on_horizontalSlider_5_valueChanged()
+{
+    int value = ui->horizontalSlider_5->value();
+    ui->horizontalSlider_2->setValue(value);
+    ui->horizontalSlider_3->setValue(value);
+    ui->horizontalSlider_4->setValue(value);
+}
