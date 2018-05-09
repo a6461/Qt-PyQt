859f35e6f6bd17d59bc876696b584a53e9845b5b
Alexander Vasilenko <a6461@yandex.ru>
2018-05-09 20:36:09 +0300

    Проект EXCEP для Python завершен; close #21

---
+++ Python/I/03-EXCEP/1/EXCEP.py
@@ -0,0 +1,24 @@
+def M1(x, y, z):
+    try:
+        a = pow(x, y)
+        print("x ^ y / z = %d" % (a / z))
+    except ZeroDivisionError as e:
+        print("ZeroDivision Exception")
+    print("M1 finished")
+    
+def M2(x, y, z):
+    try:
+        M1(x, y, z)
+    except ArithmeticError:
+        print("Arithmetic Exception")
+    print("M2 finished")
+    
+def main():
+    x = int(input("x = "))
+    y = int(input("y = "))
+    z = int(input("z = "))
+    M2(x, y, z)
+    input()
+    
+if __name__ == "__main__":
+    main()
