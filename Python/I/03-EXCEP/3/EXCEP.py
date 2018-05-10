4f7b533e9777e29982b2ad1dffaa9efda2f1ac92
Alexander Vasilenko <a6461@yandex.ru>
2018-05-11 00:31:20 +0300

    close #22

--- Python/I/03-EXCEP/3/EXCEP.py
+++ Python/I/03-EXCEP/3/EXCEP.py
@@ -3,28 +3,28 @@ import logging
 def M1(x, y, z):
     try:
         a = pow(x, y)
-        print("x ^ y / z = %d" % (a / z))
+        print('x ^ y / z = %d' % (a / z))
     except ZeroDivisionError as e:
-        print("ZeroDivision Exception")
-    print("M1 finished")
+        print('ZeroDivision Exception')
+    print('M1 finished')
     
 def M2(x, y, z):
     try:
         M1(x, y, z)
     except ArithmeticError:
-        print("Arithmetic Exception")
+        print('Arithmetic Exception')
         raise
-    print("M2 finished")
+    print('M2 finished')
     
 def main():
     try:
-        x = int(input("x = "))
-        y = int(input("y = "))
-        z = int(input("z = "))
+        x = int(input('x = '))
+        y = int(input('y = '))
+        z = int(input('z = '))
         M2(x, y, z)
     except Exception as e:
         logging.Logger('catch_all').exception(e, exc_info=False)
     input()
     
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
