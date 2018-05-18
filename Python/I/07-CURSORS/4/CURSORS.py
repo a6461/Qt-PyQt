490635cc0b9979d7491e7f93a53a6d99fcd15981
Alexander Vasilenko <a6461@yandex.ru>
2018-05-18 20:53:34 +0300

    Добавлено изменение иконки в таскбаре

--- Python/I/07-CURSORS/4/CURSORS.py
+++ Python/I/07-CURSORS/4/CURSORS.py
@@ -1,7 +1,10 @@
 from form import *
 import sys
+import ctypes
 
 def main():
+    appid = 'mycompany.myproduct.subproduct.version'
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(appid)
     app = QApplication(sys.argv)
     form = Form()
     form.show()
